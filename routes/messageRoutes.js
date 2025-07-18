2dc3997ad3f29d459b73b236694bd2f3dad2408a
silpiAkter <silpiakter461@gmail.com>
2025-07-18 19:00:48 +0600

    added route.js file

--- routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMessage, postMessage } = require('../controllers/messageController');
-
-router.get('/', getMessage);
-
-router.post('/', postMessage);
-
-module.exports = router;
---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const ProfileController = require("../Controllers/ProfileController");
+const TodoListController = require("../Controllers/ToDoListController");
+const AuthVerifyToken = require("../Middleware/AuthVerifyToken");
+const router = express.Router();
+const { getMessage, postMessage } = require('../controllers/messageController');
+
+router.get('/', getMessage);
+router.post('/', postMessage);
+router.post("/createProfile", ProfileController.CreateProfile);
+router.post("/userLogin", ProfileController.UserLogin);
+router.get("/getProfile", AuthVerifyToken, ProfileController.getProfile);
+router.post("/updateProfile", AuthVerifyToken, ProfileController.upDateProfile);
+
+
+
+module.exports = router;
